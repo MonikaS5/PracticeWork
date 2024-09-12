c749dd8616fbefeb88ec112021a841462a6a2d88
MonikaS5 <monika.bhagade@gmail.com>
2024-09-12 12:02:07 +0100

    fizzBuzz code

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,6 +1,14 @@
-const output = fizzBuzz(30);
+const output = fizzBuzz(10);
 console.log(output);
 
 function fizzBuzz(input){
+    if(typeof input !=='number')
+        return 'Not a Number';
+    if((input % 3 === 0) && (input % 5 === 0))
+        return 'FizzBuzz';
+    if(input % 3 === 0)
+        return 'Fizz';
+    if(input % 5 === 0)
+        return 'Buzz';
     return input
 }
