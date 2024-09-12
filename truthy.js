47e36271e78d8e1fd74ee7a33edfef17d31d0f25
MonikaS5 <monika.bhagade@gmail.com>
2024-09-12 22:04:00 +0100

    truthy

--- truthy.js
+++ truthy.js
@@ -1,11 +1,15 @@
-const arr = [0,1, 2, 3, 4];
+const arr = [0, 1, 2, 3, {}, [], "0", null, undefined, 5, NaN];
+
 console.log(checkTruthy(arr));
 
 function checkTruthy(arr) {
   let count = 0;
+
   for (let value of arr) {
     if (value)
-         count++;
-    return count;
+       count++;
+     
   }
+
+  return count;
 }
