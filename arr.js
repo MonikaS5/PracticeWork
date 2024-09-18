05790691633445c0c6fc2a4cf48406050ba9044f
MonikaS5 <monika.bhagade@gmail.com>
2024-09-18 13:04:29 +0100

    arr1

--- arr.js
+++ arr.js
@@ -1,10 +1,10 @@
 const numbers = [1, 2, 3, 4, 5];
 
 for(let number of numbers){
-    console.log(number);
+    console.log(number * 2);
 }
 //////
 const students = ['John', 'Mark', 'Sia'];
-students.forEach(studName=>{
-    console.log(studName);
+students.forEach((studName, index)=>{
+    console.log(index, studName);
 })
